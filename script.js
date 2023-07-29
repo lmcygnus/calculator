b0b54e4c792404e3f2e1de038f0fb2adb9657b00
skyviolet <mloida591@gmail.com>
2023-07-29 16:37:12 -0400

    calculate numbers

--- script.js
+++ script.js
@@ -1,59 +1,80 @@
-function add(a, b){
-    return a + b;
-}
-function substrac (a, b){
-    return a - b ;
-}
-function multiply (a, b){
-    return a * b;
- }
-function divide (a, b){
-    return a / b;
-  }
+let num1 = "";
+let num2 = "";
+let operator = "";
+let result = "";
 
 function operate (num1, operator, num2) {
-    if (operator === "+") return add(num1, num2);
-    if (operator === "-") return substrac(num1, num2);
-    if (operator === "x") return multiply(num1, num2);
-    if (operator === "/") return divide(num1, num2);
+    if (operator === "+") return num1 + num2;
+    if (operator === "-") return num1 - num2;
+    if (operator === "x") return num1 * num2;
+    if (operator === "/") return num1 / num2;
 }
 
-const numA = document.querySelector(".a")
-const numB = document.querySelector(".b")
-const opr = document.querySelector(".op")
-const display = document.querySelector(".result")
+function moreOps() {
+    if(num1 !== "" && operator !== "" && display2.textContent !== ""){
+        num2 = Number(display2.textContent);
+        roundResult(operate(num1, operator, num2));
+        display2.textContent = operate(num1, operator, num2);
+    }
+}
+
+function roundResult(number) {
+    return Math.round(number * 1000) / 1000
+}
+
+function clearAllnum() {
+    num1 = "";
+    num2 = "";
+    num3 = "";
+    operator = "";
+    display1.textContent = "";
+    display2.textContent = "";
+}
+
+function noPress() {
+    if(display2.textContent == ""){
+        display2.textContent = null;
+    }
+}
+
+const display1 = document.querySelector(".a")
+const display2 = document.querySelector(".b")
 const numbers = document.querySelectorAll(".number")
 const operators = document.querySelectorAll(".operator")
 const isEqual = document.querySelector(".isEqual")
 const deleteNum = document.querySelector(".delete")
-
+const clearAll = document.querySelector(".clear")
+const dot = document.querySelector(".dot")
 
 numbers.forEach((number) => {number.addEventListener("click", () => {
-    display.innerText += number.textContent;
+    display2.textContent += number.textContent;
 })})
 
 operators.forEach((op) => {op.addEventListener("click", () => {
-    opr.innerText = op.textContent
-    numA.innerText = display.textContent;
-    display.innerText = "";
+    moreOps();
+    num1 = Number(display2.textContent);
+    display1.textContent = display2.textContent;
+    operator = op.textContent;
+    display1.textContent += " " + op.textContent + " ";
+    display2.textContent = "";
 })})
 
 isEqual.addEventListener("click", () => {
-    let num1 = Number(numA.textContent);
-    let num2 = Number(display.textContent);
-    let operator = opr.textContent;
-    numB.innerText = num2;
-    display.innerText = "";
-    let result = (operate(num1, operator, num2));
-    display.innerText = result;
+    noPress();
+    num2 = Number(display2.textContent);
+    let result = roundResult(operate(num1, operator, num2));
+    if(operator === "/" && num2 === 0){result = "∞";}
+    display1.textContent += display2.textContent;
+    display2.textContent = result;
 })
 
 deleteNum.addEventListener("click", () => {
-    let newNum = display.textContent.toString().slice(0, -1)
-    display.innerText = newNum;
-    if(display.textContent === ""){
-        opr.textContent = "";
-        numA.textContent = "";
-        numB.textContent = "";
-    }
+    display2.textContent = display2.textContent.toString().slice(0, -1)
+})
+
+clearAll.addEventListener("click", clearAllnum)
+
+dot.addEventListener("click", () => {
+    if(display2.textContent.includes(".")) return;
+    display2.textContent += dot.textContent;
 })
